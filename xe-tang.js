f5485f58716750f55fe7c3a9777736ccd14d0b7d
lethihuyen1982 <you@example.com>
2021-03-14 15:12:40 +0700

    Hoan thanh bai tap

--- xe-tang.js
+++ xe-tang.js
@@ -1,27 +1,43 @@
 function moveUp() {
-    let top = document.getElementById("tank").style("top")
+    let top = document.getElementById("tank").style["top"]
     top = parseInt(top)
-    top = top - 38
-    document.getElementById("tank").style("top") = top + "px"
+    if (top > 0) {
+        top = top - 19
+        document.getElementById("tank").style["top"] = top + "px"
+        document.getElementById("tank").style["transform"] = "rotate(360deg)"
+        }
+
 }
 
 function moveDown() {
-    let top = document.getElementById("tank").style("top")
+    let top = document.getElementById("tank").style["top"]
     top = parseInt(top)
-    top = top + 38
-    document.getElementById("tank").style("top") = top + "px"
+    if (top < 171) {
+        top = top + 19
+        document.getElementById("tank").style["top"] = top + "px"
+        document.getElementById("tank").style["transform"] = "rotate(180deg)"
+    }
+
 }
 
 function moveLeft() {
-    let left = document.getElementById("tank").style("left")
+    let left = document.getElementById("tank").style["left"]
     left = parseInt(left)
-    left = left - 38
-    document.getElementById("tank").style("left") = left + "px"
+    if(left>0) {
+        left = left - 19
+        document.getElementById("tank").style["left"] = left + "px"
+        document.getElementById("tank").style["transform"] = "rotate(270deg)"
+    }
+    
 }
 
 function moveRight() {
-    let left = document.getElementById("tank").style("left")
+    let left = document.getElementById("tank").style["left"]
     left = parseInt(left)
-    left = left + 38
-    document.getElementById("tank").style("left") = left + "px"
+    if(left<171) {
+        left = left + 19
+        document.getElementById("tank").style["left"] = left + "px"
+        document.getElementById("tank").style["transform"] = "rotate(90deg)"
+    }
+    
 }
